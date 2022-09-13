eb9e7bbdbffcd8b8da1d4f25d6b7a4925fdc9135
simon <skadel@posteo.de>
2022-09-13 13:03:57 +0200

    added forEach and iife structure

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,46 @@
-let pokemonList = [
-  {
-    name: "Bulbasaur",
-    height: 0.7,
-    types: ["Grass", "Poison"],
-  },
-  {
-    name: "Charmander",
-    height: 0.6,
-    types: "fire",
-  },
-  {
-    name: "Squirtle",
-    height: 0.5,
-    types: "Water",
-  },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 0.7,
+      types: ["Grass", "Poison"],
+    },
+    {
+      name: "Charmander",
+      height: 0.6,
+      types: "fire",
+    },
+    {
+      name: "Squirtle",
+      height: 0.5,
+      types: "Water",
+    },
+  ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 0.6) {
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+
+pokemonRepository.getAll().forEach(function (list) {
+  if (list.height > 0.6) {
     document.write(
       "<p>" +
-        `--Name: ${pokemonList[i].name}; height: ${pokemonList[i].height} Wow thats Big!` +
+        `--Name: ${list.name}; height: ${list.height} Wow thats Big!` +
         "</p>"
     );
   } else {
     document.write(
-      "<p>" +
-        `--Name: ${pokemonList[i].name}; height: ${pokemonList[i].height}\n` +
-        +"</p>"
+      "<p>" + `--Name: ${list.name}; height: ${list.height}` + "</p>"
     );
   }
-}
+});
